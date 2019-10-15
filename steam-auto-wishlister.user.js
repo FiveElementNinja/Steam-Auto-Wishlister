14b2c079bde2cb00a90f1a7a258c6328cc81b416
FiveElementNinja <FiveElementNinja@users.noreply.github.com>
2019-10-15 16:30:13 -0500

    - Code cleanup. - Bug fixes for store pages with multiple Add to Card buttons, which should make the script not error out anymore. - Added script name to all console logs so you know where those messages are coming from. - Updated update and download URL fields to point to the correct URLs for automatic updating.

--- steam-auto-wishlister.user.js
+++ steam-auto-wishlister.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Steam Auto-Wishlister
 // @namespace    https://github.com/FiveElementNinja/Steam-Auto-Wishlister
-// @version      0.2
+// @version      0.3
 // @description  Automatically adds a game to your Steam wishlist when viewing its store page if it has cards.
 // @author       Five Element Ninja (@FivElementNinja)
 // @match        *://store.steampowered.com/app/*
-// @updateURL    https://github.com/FiveElementNinja/Steam-Auto-Wishlister/steam-auto-wishlister.user.js
-// @downloadURL  https://github.com/FiveElementNinja/Steam-Auto-Wishlister/steam-auto-wishlister.user.js
+// @updateURL    https://github.com/FiveElementNinja/Steam-Auto-Wishlister/raw/master/steam-auto-wishlister.user.js
+// @downloadURL  https://github.com/FiveElementNinja/Steam-Auto-Wishlister/raw/master/steam-auto-wishlister.user.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
@@ -15,7 +15,7 @@
 * Using plain Javascript because it's overkill to load in a second instance of jQuery just for this little script.
 */
 
-console.log('Loaded');
+console.log('Steam Auto-Wishlister -- Loaded');
 
 (function () {
     var category_icons = document.getElementsByClassName('category_icon');
@@ -28,10 +28,10 @@ console.log('Loaded');
         // check if this is a free app, in which case don't add it to wishlist
         var btn_addtocart = document.getElementsByClassName('btn_addtocart');
         if (btn_addtocart) {
-            for (j=0; j < btn_addtocart.length; j++) {
+            for (var j=0; j < btn_addtocart.length; j++) {
                 if (btn_addtocart[j].firstElementChild) {
-                    console.log(btn_addtocart[j].firstElementChild);
-                    if (btn_addtocart[j].firstElementChild.href.match('steam://run/')) {
+                    //console.log(btn_addtocart[j].firstElementChild);
+                    if (btn_addtocart[j].firstElementChild.href && btn_addtocart[j].firstElementChild.href.match('steam://run/')) {
                         is_free = true;
                         break;
                     }
@@ -40,14 +40,14 @@ console.log('Loaded');
         }
         
         // check if it has cards
-        for (i=0; i < category_icons.length; i++) {
-            if (category_icons[i].src.match('ico_cards')) {
+        for (var i=0; i < category_icons.length; i++) {
+            if (category_icons[i].src && category_icons[i].src.match('ico_cards')) {
                 has_cards = true;
                 break;
             }
             
             // check if this is a DLC app, in which case don't add it to wishlist
-            if (category_icons[i].src.match('ico_dlc')) {
+            if (category_icons[i].src && category_icons[i].src.match('ico_dlc')) {
                 is_dlc = true;
                 break;
             }
@@ -55,7 +55,7 @@ console.log('Loaded');
 
         // if it does have cards and isn't a F2P app or DLC, add it to the wishlist
         if (has_cards && !is_free && !is_dlc) {
-            console.log('This app has cards. Adding to Wishlist.');
+            console.log('Steam Auto-Wishlister -- This app has cards. Adding to Wishlist.');
 
             var wishlist_area = document.getElementById('add_to_wishlist_area');
             if (wishlist_area && wishlist_area.firstElementChild && wishlist_area.firstElementChild.href && wishlist_area.firstElementChild.href != '' && wishlist_area.firstElementChild.href.indexOf('javascript:AddToWishlist(') === 0) {
@@ -66,15 +66,15 @@ console.log('Loaded');
                     // highlight the On Wishlist button to indicate that it has been auto-clicked
                     document.getElementsByClassName('queue_btn_active')[0].style.border ='1px solid #999999';
                 } catch (err) {
-                    console.log('Error adding to Wishlist. Try reloading the page.');
+                    console.log('Steam Auto-Wishlister -- Error adding to Wishlist. Try reloading the page.');
                 }
             } else {
-                console.log('This app is already on your Wishlist or there was an error.');
+                console.log('Steam Auto-Wishlister -- This app is already on your Wishlist or there was an error.');
             }
         } else {
-            console.log('This app does not have cards.');
+            console.log('Steam Auto-Wishlister -- This app does not have cards.');
         }
     } else {
-        console.log('Script error.');
+        console.log('Steam Auto-Wishlister -- Script error.');
     }
 }());
